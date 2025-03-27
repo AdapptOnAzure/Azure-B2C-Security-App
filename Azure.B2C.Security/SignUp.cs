5a2cf8ef4eb42e57f1dff5dfe5fe98e2fa7859eb
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2025-03-27 20:44:10 +0100

    feat: ready endpoints

--- Azure.B2C.Security/SignUp.cs
+++ Azure.B2C.Security/SignUp.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Graph;
-using Microsoft.Graph.Models;
+using System.Text.Json;
+using System.Net.Mail;
 
 namespace Azure.B2C.Security;
 
@@ -10,13 +9,45 @@ public static class SignUp
    {
       RouteGroupBuilder group = endpoints.MapGroup("/signup");
 
-      group.MapPost("/bad-email", BadEmail).WithOpenApi();
+      group.MapPost("/bad-email", BadEmail);
+      group.MapPost("/bad-host", BadHost);
+      group.MapPost("/too-many-users", TooManyUsers);
 
       return endpoints;
    }
 
-   private static IResult BadEmail([FromBody] User user)
+   private static async ValueTask<IResult> BadEmail(HttpContext context)
    {
-      return TypedResults.BadRequest();
+      var user = await JsonSerializer.DeserializeAsync<Dictionary<string, JsonElement?>>(context.Request.Body);
+
+      if (user is null 
+          || !user.TryGetValue("email", out JsonElement? emailObj)
+          || emailObj is not { ValueKind: JsonValueKind.String }
+          || !MailAddress.TryCreate(emailObj.Value.ToString(), out MailAddress? email)
+          )
+      {
+         return TypedResults.BadRequest();
+      }
+
+      return TypedResults.Ok(new Response());
+   }
+
+   private static IResult BadHost(HttpContext context)
+   {
+      string remoteIp = context.Connection.RemoteIpAddress?.ToString() ?? string.Empty;
+      return TypedResults.Ok(new Response());
+   }
+   
+   private static IResult TooManyUsers(HttpContext context)
+   {
+      return TypedResults.Ok(new Response());
    }
 }
+
+public enum Action
+{
+   ShowBlockPage,
+   Continue,
+}
+
+public record Response(string Version = "1.0.0", string Action = "ShowBlockPage");
