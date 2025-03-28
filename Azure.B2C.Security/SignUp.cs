779be9d74307b85e7ecf91dc552b26392fa45215
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2025-03-28 13:18:27 +0100

    feat: add captcha endpoint

--- Azure.B2C.Security/SignUp.cs
+++ Azure.B2C.Security/SignUp.cs
@@ -6,6 +6,12 @@ public static class SignUp
 {
     private const string Version = "1.0.0";
 
+    private static HttpClient _httpClient = new()
+    {
+        BaseAddress = new Uri("https://www.google.com/recaptcha/api/siteverify"),
+        Timeout = TimeSpan.FromSeconds(15),
+    };
+
     public static IEndpointRouteBuilder MapSignUp(this IEndpointRouteBuilder endpoints)
     {
         RouteGroupBuilder group = endpoints.MapGroup("/signup");
@@ -13,6 +19,7 @@ public static class SignUp
         group.MapPost("/bad-email", BadEmail);
         group.MapPost("/bad-host", BadHost);
         group.MapPost("/too-many-users", TooManyUsers);
+        group.MapPost("/captcha", Captcha);
 
         return endpoints;
     }
@@ -58,6 +65,72 @@ public static class SignUp
     private static IResult TooManyUsers(HttpContext context)
     {
         return TypedResults.Ok();
+    }
+
+    private static async ValueTask<IResult> Captcha(HttpContext context, IConfiguration config)
+    {
+        var user = await JsonSerializer.DeserializeAsync<Dictionary<string, JsonElement?>>(
+            context.Request.Body
+        );
+
+        if (user is null)
+        {
+            return TypedResults.BadRequest();
+        }
+
+        string key = $"extension_{config["AppId"]}_CaptchaUserResponseToken";
+
+        if (
+            !user.TryGetValue(key, out JsonElement? token)
+            || token is not { ValueKind: JsonValueKind.String }
+        )
+        {
+            return TypedResults.BadRequest(
+                new
+                {
+                    Version,
+                    Status = 400,
+                    Action = Action.ValidationError.ToString(),
+                    UserMessage = "Please complete the Captcha.",
+                }
+            );
+        }
+
+        if (!await RunCaptcha(token.Value.ToString(), config))
+        {
+            return TypedResults.BadRequest(
+                new
+                {
+                    Version,
+                    Status = 400,
+                    Action = Action.ValidationError.ToString(),
+                    UserMessage = "Captcha validation failed. Please try again.",
+                }
+            );
+        }
+
+        return TypedResults.Ok(new {Version, Action = Action.Continue.ToString()});
+    }
+
+    private static async ValueTask<bool> RunCaptcha(string token, IConfiguration config)
+    {
+        try
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync(
+                (string?)null,
+                new { Secret = config["CaptchaSecret"], Response = token }
+            );
+            response.EnsureSuccessStatusCode();
+
+            var data = await response.Content.ReadFromJsonAsync<Dictionary<string, JsonElement?>>();
+
+            return data is not null && data.TryGetValue("success", out JsonElement? successObj) &&
+                   successObj is not null && bool.TryParse(successObj.Value.ToString(), out bool success) && success;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
 
