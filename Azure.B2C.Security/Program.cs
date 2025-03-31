7c27ac443728ba853943246ce88a76fe4e46c20b
the-gabe <105918853+the-gabe@users.noreply.github.com>
2025-03-31 18:06:24 +0100

    fix: Resolve ASP0019 warnings

--- Azure.B2C.Security/Program.cs
+++ Azure.B2C.Security/Program.cs
@@ -26,17 +26,17 @@ app.Use(async (context, next) =>
     context.Response.OnStarting(() =>
     {
         // Yes this is probably overkill and by nature of this application, mostly redundant.
-        context.Response.Headers.Add("strict-transport-security", "max-age=63072000; includeSubDomains; preload");
-        context.Response.Headers.Add("x-content-type-options", "nosniff");
-        context.Response.Headers.Add("referrer-policy", "no-referrer");
-        context.Response.Headers.Add("cross-origin-opener-policy", "same-origin");
-        context.Response.Headers.Add("cross-origin-embedder-policy", "require-corp");
-        context.Response.Headers.Add("cross-origin-resource-policy", "same-origin");
-        context.Response.Headers.Add("origin-agent-cluster", "?1");
-        context.Response.Headers.Add("x-frame-options", "DENY");
-        context.Response.Headers.Add("x-xss-protection", "0");
-        context.Response.Headers.Add("content-security-policy", "default-src 'none'; connect-src 'none'; frame-ancestors 'none'; form-action 'none'; sandbox; base-uri 'none'");
-        context.Response.Headers.Add("permissions-policy", "accelerometer=(), autoplay=(), bluetooth=(), camera=(), ch-device-memory=(), ch-downlink=(), ch-dpr=(), ch-ect=(), ch-prefers-color-scheme=(), ch-prefers-reduced-motion=(), ch-prefers-reduced-transparency=(), ch-rtt=(), ch-save-data=(), ch-ua=(), ch-ua-arch=(), ch-ua-bitness=(), ch-ua-form-factors=(), ch-ua-full-version=(), ch-ua-full-version-list=(), ch-ua-mobile=(), ch-ua-model=(), ch-ua-platform=(), ch-ua-platform-version=(), ch-ua-wow64=(), ch-viewport-height=(), ch-viewport-width=(), ch-width=(), clipboard-read=(), clipboard-write=(), compute-pressure=(), cross-origin-isolated=(), display-capture=(), encrypted-media=(), ethereum=(), fullscreen=(), geolocation=(), gyroscope=(), hid=(), identity-credentials-get=(), idle-detection=(), keyboard-map=(), local-fonts=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-create=(), publickey-credentials-get=(), screen-wake-lock=(), serial=(), solana=(), storage-access=(), sync-xhr=(), usb=(), web-share=(), window-management=(), xr-spatial-tracking=(), gamepad=(), unload=()");
+        context.Response.Headers.Append("strict-transport-security", "max-age=63072000; includeSubDomains; preload");
+        context.Response.Headers.Append("x-content-type-options", "nosniff");
+        context.Response.Headers.Append("referrer-policy", "no-referrer");
+        context.Response.Headers.Append("cross-origin-opener-policy", "same-origin");
+        context.Response.Headers.Append("cross-origin-embedder-policy", "require-corp");
+        context.Response.Headers.Append("cross-origin-resource-policy", "same-origin");
+        context.Response.Headers.Append("origin-agent-cluster", "?1");
+        context.Response.Headers.Append("x-frame-options", "DENY");
+        context.Response.Headers.Append("x-xss-protection", "0");
+        context.Response.Headers.Append("content-security-policy", "default-src 'none'; connect-src 'none'; frame-ancestors 'none'; form-action 'none'; sandbox; base-uri 'none'");
+        context.Response.Headers.Append("permissions-policy", "accelerometer=(), autoplay=(), bluetooth=(), camera=(), ch-device-memory=(), ch-downlink=(), ch-dpr=(), ch-ect=(), ch-prefers-color-scheme=(), ch-prefers-reduced-motion=(), ch-prefers-reduced-transparency=(), ch-rtt=(), ch-save-data=(), ch-ua=(), ch-ua-arch=(), ch-ua-bitness=(), ch-ua-form-factors=(), ch-ua-full-version=(), ch-ua-full-version-list=(), ch-ua-mobile=(), ch-ua-model=(), ch-ua-platform=(), ch-ua-platform-version=(), ch-ua-wow64=(), ch-viewport-height=(), ch-viewport-width=(), ch-width=(), clipboard-read=(), clipboard-write=(), compute-pressure=(), cross-origin-isolated=(), display-capture=(), encrypted-media=(), ethereum=(), fullscreen=(), geolocation=(), gyroscope=(), hid=(), identity-credentials-get=(), idle-detection=(), keyboard-map=(), local-fonts=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-create=(), publickey-credentials-get=(), screen-wake-lock=(), serial=(), solana=(), storage-access=(), sync-xhr=(), usb=(), web-share=(), window-management=(), xr-spatial-tracking=(), gamepad=(), unload=()");
         return Task.FromResult(0);
     });
 
