7c7c1be0bea4196bf7e6efddbb13941b08a2f865
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2025-04-01 22:51:45 +0200

    feat: extract B2C API Connector logic into a separate utility

---
+++ Azure.B2C.Security/Utils/B2CConnector.cs
@@ -0,0 +1,15 @@
+namespace Azure.B2C.Security.Utils;
+
+public static class B2CConnector
+{
+    public const string Version = "1.0.0";
+    public const string BlockingResponseMessage =
+        "There was a problem with your request. You are not able to sign up at this time. Please contact your system administrator";
+
+    public enum Action
+    {
+        ShowBlockPage,
+        Continue,
+        ValidationError,
+    }
+}
